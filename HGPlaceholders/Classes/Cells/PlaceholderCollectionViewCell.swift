8ee6e3ef6a59bc1111c8d147fc633ec95fa4ac5e
iwasa koya <>
2019-06-04 14:39:39 +0900

    fix bugs

--- HGPlaceholders/Classes/Cells/PlaceholderCollectionViewCell.swift
+++ HGPlaceholders/Classes/Cells/PlaceholderCollectionViewCell.swift
@@ -33,4 +33,46 @@ open class PlaceholderCollectionViewCell: UICollectionViewCell {
 
 extension PlaceholderCollectionViewCell: Reusable {}
 extension PlaceholderCollectionViewCell: NibLoadable {}
-extension PlaceholderCollectionViewCell: CellPlaceholding {}
+extension PlaceholderCollectionViewCell: CellPlaceholding {
+    
+    ///  Changes the cell style to match placeholder style
+    ///
+    /// - Parameters:
+    ///   - style: the style to apply
+    ///   - tintColor: the tint color, is used for some items when the style color is nil
+    public func apply(style: PlaceholderStyle, tintColor: UIColor?) {
+        cellView.backgroundColor = style.backgroundColor
+        
+        let buttonBackgroundColor = style.actionBackgroundColor ?? tintColor
+        actionButton?.backgroundColor = buttonBackgroundColor
+        
+        let actionColor = style.actionTitleColor
+        actionButton?.setTitleColor(actionColor, for: .normal)
+        actionButton?.titleLabel?.font = style.actionTitleFont
+        
+        activityIndicator?.color = style.activityIndicatorColor
+        
+        titleLabel?.textColor = style.titleColor
+        titleLabel?.font = style.titleFont
+        titleLabel?.textAlignment = style.titleTextAlignment
+        
+        subtitleLabel?.textColor = style.subtitleColor
+        subtitleLabel?.font = style.subtitleFont
+        subtitleLabel?.textAlignment = style.subtitleTextAlignment
+    }
+    
+    ///  Sets in the cell the placeholder texts, image, ...
+    ///
+    /// - Parameter data: the data of the cell (texts, images, etc)
+    public func apply(data: PlaceholderData?) {
+        actionButton?.setTitle(data?.action, for: .normal)
+        actionButton?.isHidden = (data?.action == nil)
+        
+        titleLabel?.text = data?.title
+        subtitleLabel?.text = data?.subtitle
+        placeholderImageView?.image = data?.image
+        
+        data?.showsLoading == true ? activityIndicator?.startAnimating() : activityIndicator?.stopAnimating()
+    }
+    
+}
